6a0e42655d2f7d01145d84d06b06e6b0888266b9
Maksym Shepeta <maksim.shepeta@gmail.com>
2024-10-08 15:44:00 +0200

    some comments

--- obj/Debug/net8.0/dotNET-lab-1.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotNET-lab-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotNET-lab-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f144838a420c61e91dcc4b8c6c4b2f59d8fc6328")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8e2dfd7dacfc22cea5c0de4959c2e11db75f620")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotNET-lab-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotNET-lab-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
